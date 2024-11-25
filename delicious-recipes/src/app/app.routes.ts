f0b3a3569780483e645ed836c6f2aef6a689aba6
unknown <sadface_@abv.bg>
2024-11-25 17:59:54 +0200

    Add app config and routes

---
+++ delicious-recipes/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { AuthFormComponent } from './features/user-auth/user-auth.component';
+import { DetailsComponent } from './features/details/details.component';
+import { CreateEditComponent } from './features/create-edit/create-edit.component';
+import { CatalogComponent } from './features/catalog/catalog.component';
+
+export const routes: Routes = [
+    { path: 'catalog', component: CatalogComponent },
+    { path: 'create', component: CreateEditComponent },
+    { path: 'login', component: AuthFormComponent },
+    { path: 'register', component: AuthFormComponent },
+    { path: ':recipeId/details', component: DetailsComponent },
+    { path: ':recipeId/edit', component: CreateEditComponent }
+];
