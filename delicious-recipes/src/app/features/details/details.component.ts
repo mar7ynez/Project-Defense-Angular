7c38c746bc040dc2bc00100e526599f30f63ac29
unknown <sadface_@abv.bg>
2024-11-28 12:10:50 +0200

    Handle recipe details functionality and conditionally render based on user authentication

--- delicious-recipes/src/app/features/details/details.component.ts
+++ delicious-recipes/src/app/features/details/details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CatalogService } from '../../core/services/recipe.service';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Recipe } from '../../shared/types/recipe';
+import { User } from '../../shared/types/user';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-details',
@@ -12,14 +14,56 @@ import { Recipe } from '../../shared/types/recipe';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent implements OnInit {
-  recipeDetails: Recipe | null = null
+  recipeData: Recipe | null = null
+  recipeId: string = '';
+  isOwner: boolean = false;
+  hasLiked: boolean = true;
 
-  constructor(private catalogService: CatalogService, private router: ActivatedRoute) { }
+  constructor(
+    private catalogService: CatalogService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.catalogService.getOne(this.router.snapshot.params['recipeId']).subscribe({
-      next: recipeDetails => {
-        this.recipeDetails = recipeDetails
+    this.recipeId = this.activatedRoute.snapshot.params['recipeId'];
+    this.fetchRecipeData();
+  }
+
+  get userData(): User | null {
+    return this.authService.user;
+  }
+
+  onDelete(): void {
+    this.catalogService.del(this.recipeId).subscribe({
+      next: () => {
+        this.router.navigate(['/catalog']);
+      },
+      error: (error) => {
+        alert('Error deleting the recipe!');
+      }
+    });
+  }
+
+  onLike(): void {
+    this.catalogService.like(this.recipeId).subscribe({
+      next: () => {
+        this.router.navigate(['/', this.recipeId, 'details'])
+          .then(() => this.fetchRecipeData());
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    })
+  }
+
+  fetchRecipeData(): void {
+    this.catalogService.getOne(this.recipeId).subscribe({
+      next: recipeData => {
+        this.recipeData = recipeData
+
+        this.isOwner = recipeData._ownerId === this.userData?._id
+        this.hasLiked = recipeData.likes.includes(this.userData?._id ?? '');
       },
       error: error => {
         alert('Error fetching recipe data!');
