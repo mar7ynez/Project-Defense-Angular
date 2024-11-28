ad162be56393c6106cd7a90404205515326d01c4
unknown <sadface_@abv.bg>
2024-11-28 11:59:37 +0200

    Update recipe service

--- delicious-recipes/src/app/core/services/recipe.service.ts
+++ delicious-recipes/src/app/core/services/recipe.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
-import { api, host } from "../../../environments/environment";
-import { Injectable } from "@angular/core";
+import { endpoints, host } from "../../../environments/environment";
+import { Injectable, signal } from "@angular/core";
 import { Recipe } from "../../shared/types/recipe";
 
 @Injectable()
@@ -9,18 +9,26 @@ export class CatalogService {
     constructor(private http: HttpClient) { }
 
     getRecipes() {
-        return this.http.get<Recipe[]>(`${host}${api.getAll}`);
+        return this.http.get<Recipe[]>(`${host}${endpoints.getAllRecipes}`);
     }
 
     getOne(recipeId: string) {
-        return this.http.get<Recipe>(`${host}${api.getOne(recipeId)}`);
+        return this.http.get<Recipe>(`${host}${endpoints.getOneRecipe(recipeId)}`);
     }
 
     edit(updatedRecipe: Recipe, recipeId: string) {
-        return this.http.put<Recipe>(`${host}${api.getOne(recipeId)}`, updatedRecipe);
+        return this.http.put<Recipe>(`${host}${endpoints.getOneRecipe(recipeId)}`, updatedRecipe);
     }
 
     create(recipeData: Recipe) {
-        return this.http.post<Recipe>(`${host}${api.getAll}`, recipeData);
+        return this.http.post<Recipe>(`${host}${endpoints.getAllRecipes}`, recipeData);
+    }
+
+    del(recipeId: string) {
+        return this.http.delete(`${host}${endpoints.getOneRecipe(recipeId)}`);
+    }
+
+    like(recipeId: string) {
+        return this.http.get(`${host}${endpoints.likeRecipe(recipeId)}`);
     }
 }
