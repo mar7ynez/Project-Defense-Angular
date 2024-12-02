d4915e40de99b9faa434510b58e41dda0bbf783b
unknown <sadface_@abv.bg>
2024-12-02 18:10:23 +0200

    Import authetication component and remove providers

--- delicious-recipes/src/app/app.component.ts
+++ delicious-recipes/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './shared/components/navigation/navigation.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { AuthenticationComponent } from './features/authentication/authentication.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent, FooterComponent],
+  imports: [RouterOutlet, NavigationComponent, FooterComponent, AuthenticationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
