228613fedcd66a7ac751477dbcd5fe1ef8737687
unknown <sadface_@abv.bg>
2024-12-02 18:13:22 +0200

    Add route for home component

--- delicious-recipes/src/app/app.routes.ts
+++ delicious-recipes/src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { AuthFormComponent } from './features/user-auth/user-auth.component';
 import { DetailsComponent } from './features/details/details.component';
 import { CreateEditComponent } from './features/create-edit/create-edit.component';
 import { CatalogComponent } from './features/catalog/catalog.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'catalog', component: CatalogComponent },
     { path: 'create', component: CreateEditComponent },
     { path: 'login', component: AuthFormComponent },
