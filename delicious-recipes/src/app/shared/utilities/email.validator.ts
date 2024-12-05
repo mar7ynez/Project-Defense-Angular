a3390d5fb4b3f9b77ffa5ccce7d3942a65365aaa
unknown <sadface_@abv.bg>
2024-12-05 23:54:54 +0200

    Updated the error key to 'pattern' when the email format does not match the specified regex pattern.

--- delicious-recipes/src/app/shared/utilities/email.validator.ts
+++ delicious-recipes/src/app/shared/utilities/email.validator.ts
@@ -3,5 +3,5 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 export const emailValidator = (control: AbstractControl): ValidationErrors | null => {
     const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-    return !emailPattern.test(control.value) ? { emailPattern: 'Invalid Email' } : null;
+    return !emailPattern.test(control.value) ? { pattern: 'Invalid Email' } : null;
 }
