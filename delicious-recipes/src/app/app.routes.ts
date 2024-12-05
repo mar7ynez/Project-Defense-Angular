4bdce9ac5b5c8c4b42282a0641dc566132a4d6ad
unknown <sadface_@abv.bg>
2024-12-05 23:57:22 +0200

    Added a new route for the profile component.

--- delicious-recipes/src/app/app.routes.ts
+++ delicious-recipes/src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { DetailsComponent } from './features/details/details.component';
 import { CreateEditComponent } from './features/create-edit/create-edit.component';
 import { CatalogComponent } from './features/catalog/catalog.component';
 import { HomeComponent } from './features/home/home.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { ProfileComponent } from './features/profile/profile.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'catalog', component: CatalogComponent },
-    { path: 'create', component: CreateEditComponent },
+    { path: 'create', component: CreateEditComponent, canActivate: [AuthGuard] },
     { path: 'login', component: AuthFormComponent },
     { path: 'register', component: AuthFormComponent },
+    { path: 'profile', component: ProfileComponent },
     { path: ':recipeId/details', component: DetailsComponent },
-    { path: ':recipeId/edit', component: CreateEditComponent }
+    { path: ':recipeId/edit', component: CreateEditComponent },
 ];
