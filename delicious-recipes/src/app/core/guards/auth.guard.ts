1b9c3a57b2e46d7cc5061e0b1715b0095b897269
unknown <sadface_@abv.bg>
2024-12-08 01:57:05 +0200

    Implement AuthGuard to protect routes and redirect unauthenticated users to login

---
+++ delicious-recipes/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { catchError, map, Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+
+    constructor(private authService: AuthService, private router: Router) { }
+
+    canActivate(): Observable<boolean> {
+        return this.authService.getProfile().pipe(
+            map(user => {
+                if (user) {
+                    return true;
+                } else {
+                    this.router.navigate(['/login']);
+                    return false;
+                }
+            })
+        );
+    }
+}
