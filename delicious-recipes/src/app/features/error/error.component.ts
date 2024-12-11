80f0c60258bd9e35d080e48f729e330c4cd8bd17
unknown <sadface_@abv.bg>
2024-12-11 21:48:00 +0200

    Add error component to display handled errors

---
+++ delicious-recipes/src/app/features/error/error.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, signal } from "@angular/core";
+import { ErrorService } from "../../core/services/error.service";
+import { ApiError } from "../../shared/types/error";
+
+@Component({
+    selector: 'app-error',
+    standalone: true,
+    templateUrl: './error.component.html',
+    styleUrl: './error.component.css',
+})
+
+export class ErrorComponent implements OnInit {
+    errorMessage = signal('');
+    private timeoutId: any;
+
+    constructor(private errorService: ErrorService) { }
+
+    ngOnInit(): void {
+        this.errorService.apiError$.subscribe((error: string | '') => {
+            if (error) {
+                this.errorMessage.set(error);
+
+                if (this.timeoutId) {
+                    clearTimeout(this.timeoutId);
+                }
+
+                this.timeoutId = setTimeout(() => {
+                    this.errorMessage.set('');
+                }, 5000);
+            } else {
+                this.errorMessage.set('');
+            }
+        });
+    }
+}
