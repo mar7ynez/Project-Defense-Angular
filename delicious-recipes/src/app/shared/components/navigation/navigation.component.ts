88be77e60dbe90936cd2748f90e99d47e15610a6
unknown <sadface_@abv.bg>
2024-12-11 22:01:04 +0200

    Add dropdown menu for improved navigation to user profile and My Recipess

--- delicious-recipes/src/app/shared/components/navigation/navigation.component.ts
+++ delicious-recipes/src/app/shared/components/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
 import { AuthService } from "../../../core/services/auth.service";
+import { User } from "../../types/user";
 
 @Component({
     selector: 'app-navigation',
@@ -10,20 +11,48 @@ import { AuthService } from "../../../core/services/auth.service";
     styleUrl: './navigation.component.css'
 })
 
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+    isProfileToggled: boolean = false;
+    profile: User | null = null;
+
     constructor(private authService: AuthService, private router: Router) { }
+
+    ngOnInit(): void {
+        this.authService.user$.subscribe({
+            next: (profile) => {
+                this.profile = profile;
+            }
+        });
+    }
 
     get authenticationService() {
         return this.authService;
     }
 
+    onProfile(event: Event) {
+        event.stopPropagation();
+
+        this.isProfileToggled = !this.isProfileToggled;
+    }
+
     onLogout(e: Event): void {
         e.preventDefault();
 
+        this.isProfileToggled = false;
+
         this.authService.logout().subscribe({
             next: () => {
-                this.router.navigate(['/login']);
+                this.router.navigate(['/']);
             }
         })
+    }
+
+    @HostListener('document:click', ['$event'])
+
+    onDocumentClick(event: MouseEvent) {
+        const menuElement = document.querySelector('.sub-menu-wrapper');
+        if (menuElement && !menuElement.contains(event.target as Node) && this.isProfileToggled) {
+            this.isProfileToggled = false;
+        }
     }
 }
