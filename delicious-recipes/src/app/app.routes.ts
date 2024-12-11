74a0467a5014e24666a385e1124df5cd49a5d23e
unknown <sadface_@abv.bg>
2024-12-11 21:27:44 +0200

    Add dashboard route and integrate UserAccessGuard

--- delicious-recipes/src/app/app.routes.ts
+++ delicious-recipes/src/app/app.routes.ts
@@ -7,15 +7,20 @@ import { HomeComponent } from './features/home/home.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { ProfileComponent } from './features/profile/profile.component';
 import { NotFound } from './features/not-found/not-found.component';
+import { MyRecipes } from './features/my-recipes/my-recipes.component';
+import { UserAccessGuard } from './core/guards/userAccess.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'catalog', component: CatalogComponent },
+    { path: 'dashboard', component: MyRecipes, canActivate: [AuthGuard] },
     { path: 'create', component: CreateEditComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: AuthFormComponent },
-    { path: 'register', component: AuthFormComponent },
+    { path: 'login', component: AuthFormComponent, canActivate: [UserAccessGuard] },
+    { path: 'register', component: AuthFormComponent, canActivate: [UserAccessGuard] },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
     { path: ':recipeId/details', component: DetailsComponent },
     { path: ':recipeId/edit', component: CreateEditComponent, canActivate: [AuthGuard] },
+
+    { path: '404', component: NotFound },
     { path: '**', component: NotFound }
 ];
