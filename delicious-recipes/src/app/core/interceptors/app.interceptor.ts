f077e136568555663835aa694faa5823cb167692
unknown <sadface_@abv.bg>
2024-12-11 21:31:19 +0200

    Add error handling to the interceptor redirect on 401, navigate to home on other errors

--- delicious-recipes/src/app/core/interceptors/app.interceptor.ts
+++ delicious-recipes/src/app/core/interceptors/app.interceptor.ts
@@ -1,10 +1,29 @@
 import { HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError } from "rxjs";
+import { ErrorService } from "../services/error.service";
 
 export const appInterceptor: HttpInterceptorFn = (request, next) => {
+    const router = inject(Router);
+    const errorService = inject(ErrorService);
 
     request = request.clone({
         withCredentials: true
     });
 
-    return next(request);
+    return next(request).pipe(
+        catchError((error) => {
+
+            errorService.setError(error.error.error);
+
+            if (error.status === 401) {
+                router.navigate([router.url]);
+            } else {
+                router.navigate(['/']);
+            }
+
+            return [error]
+        })
+    );
 }
